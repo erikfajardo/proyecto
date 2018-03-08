b760b33e911a3546d0dbebc5d3d7625e02be771d
erikfajardo <erikfajardo_8@hotmail.com>
2018-03-07 20:20:14 -0600

    modulos de sesion

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
 $('#contenido').load('modulos/inicio/index.html');
 
-$("#boton").click(function(e) {
+$("#irInicio").click(function(e) {
+  $('#contenido').load('modulos/inicio/index.html');
+})
+  $('#modulos').load('modulos/sesion/registrar.html');
+});
+
+$("#registro").click(function(e) {
   swal({
     confirmButtonText: 'ACEPTAR',
     showCancelButton: true,
@@ -11,8 +17,31 @@ $("#boton").click(function(e) {
     allowOutsideClick: false
   }).then((result) => {
   if (result.value) {
+    swal({
+      confirmButtonText: 'ACEPTAR',
+      title:'Registro Exitoso',
+      type:'success'
+    })
 }
 })
   $('#modulos').load('modulos/sesion/registrar.html');
 });
+
+$("#inicio").click(function(e) {
+  swal({
+    confirmButtonText: 'ACEPTAR',
+    showCancelButton: true,
+    cancelButtonColor: '#DB2828',
+    cancelButtonText: 'Cancelar',
+    html: "<div id='modulos'></div>",
+    allowOutsideClick: false
+  }).then((result) => {
+  if (result.value) {
+  $('#contenido').load('modulos/usuario/index.html');
+}
+})
+  $('#modulos').load('modulos/sesion/iniciar.html');
+});
+
+
 });
