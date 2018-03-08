0fdfa7d6f5a5f8617c1da34e37bbf9c31e25e770
erikfajardo <erikfajardo_8@hotmail.com>
2018-03-07 20:00:16 -0600

    integracion sweet alert

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
 $('#contenido').load('modulos/inicio/index.html');
+
+$("#boton").click(function(e) {
+  swal({
+    confirmButtonText: 'ACEPTAR',
+    showCancelButton: true,
+    cancelButtonColor: '#DB2828',
+    cancelButtonText: 'Cancelar',
+    html: "<div id='modulos'></div>",
+    allowOutsideClick: false
+  }).then((result) => {
+  if (result.value) {
+}
+})
+  $('#modulos').load('modulos/sesion/registrar.html');
+});
 });
