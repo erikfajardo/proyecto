ead5b43653719b39c84d776af169556246217ff4
erikfajardo <erikfajardo_8@hotmail.com>
2018-03-07 21:38:56 -0600

    Modulo subir video

--- js/usuario.js
+++ js/usuario.js
@@ -13,12 +13,32 @@ $("#editarPerfil").click(function(e) {
   if (result.value) {
     swal({
       confirmButtonText: 'ACEPTAR',
-      title:'Registro Exitoso',
+      title:'Cambio Exitoso',
       type:'success'
     })
 }
 })
   $('#modulos').load('modulos/usuario/editar.html');
+});
+
+$("#subirVideo").click(function(e) {
+  swal({
+    confirmButtonText: 'ACEPTAR',
+    showCancelButton: true,
+    cancelButtonColor: '#DB2828',
+    cancelButtonText: 'Cancelar',
+    html: "<div id='modulos'></div>",
+    allowOutsideClick: false
+  }).then((result) => {
+  if (result.value) {
+    swal({
+      confirmButtonText: 'ACEPTAR',
+      title:'Video Subido con Exito',
+      type:'success'
+    })
+}
+})
+  $('#modulos').load('modulos/subirVideo/index.html');
 });
 
 });
