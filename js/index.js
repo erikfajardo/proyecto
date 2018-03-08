4bead287e653c420f7095c4ea9bb3b144face0ee
erikfajardo <erikfajardo_8@hotmail.com>
2018-03-08 07:31:42 -0600

    Cambios

--- js/index.js
+++ js/index.js
@@ -41,9 +41,30 @@ $("#inicio").click(function(e) {
   }).then((result) => {
   if (result.value) {
   $('#contenido').load('modulos/usuario/index.html');
+ $('#usuarioPanel').html("<input type='search' name='' value='' placeholder='Buscar...' class='buscardorPanel' id='buscador'><input type='submit' value='Buscar' class='botonBuscar' id='busqueda'><a class='blanco' href='#' id='registro'>Bienvenido USUARIO</a> / <a class='blanco' href='#' id='cerrarSesion'>Cerrar Sesión</a>");
+
+
 }
 })
   $('#modulos').load('modulos/sesion/iniciar.html');
+});
+
+$("#cerrarSesion").click(function(e) {
+  swal({
+    title: 'Seguro de Cerrar Sesión',
+    confirmButtonText: 'ACEPTAR',
+    showCancelButton: true,
+    cancelButtonColor: '#DB2828',
+    cancelButtonText: 'Cancelar',
+    allowOutsideClick: false
+  }).then((result) => {
+  if (result.value) {
+  $('#contenido').load('modulos/inicio/index.html');
+ $('#usuarioPanel').html("<input type='search' name='' value='' placeholder='Buscar...' class='buscardorPanel' id='buscador'><input type='submit' value='Buscar' class='botonBuscar' id='busqueda'><a class='blanco' href='#' id='registro'>Registrarse</a> / <a class='blanco' href='#' id='inicio'>Iniciar Sesión</a>");
+
+
+}
+})
 });
 
 });
